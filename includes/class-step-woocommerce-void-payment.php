58b3b06c10e7c1c18396921c48dd62831aa8c8d3
Yoren Chang <yorenchang@gmail.com>
2018-07-25 20:43:01 +0800

    Add void payment step

---
+++ includes/class-step-woocommerce-void-payment.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * Gravity Flow WooCommerce Void Step
+ *
+ * @package     GravityFlow
+ * @subpackage  Classes/Step
+ * @copyright   Copyright (c) 2015-2018, Steven Henty S.L.
+ * @license     http://opensource.org/licenses/gpl-3.0.php GNU Public License
+ * @since       1.0.0-dev
+ */
+
+if ( class_exists( 'Gravity_Flow_Step' ) ) {
+
+	class Gravity_Flow_Step_Woocommerce_Void_Payment extends Gravity_Flow_Step {
+		/**
+		 * A unique key for this step type.
+		 *
+		 * @var string
+		 */
+		public $_step_type = 'woocommerce_void_payment';
+
+		/**
+		 * Set a custom icon in the step settings.
+		 * 32px x 32px
+		 *
+		 * @since 1.0.0-dev
+		 *
+		 * @return string
+		 */
+		public function get_icon_url() {
+			return '<i class="woocommerce" aria-hidden="true"></i>';
+		}
+
+		/**
+		 * Returns the label for the step.
+		 *
+		 * @since 1.0.0-dev
+		 *
+		 * @return string
+		 */
+		public function get_label() {
+			return esc_html__( 'Void Payment', 'gravityflowwoocommerce' );
+		}
+
+		/**
+		 * Is this step supported on this server? Override to hide this step in the list of step types if the requirements are not met.
+		 *
+		 * @since 1.0.0-dev
+		 *
+		 * @return bool
+		 */
+		public function is_supported() {
+			$form_id  = $this->get_form_id();
+			$form     = GFAPI::get_form( $form_id );
+			$settings = rgar( $form, 'gravityflowwoocommerce' );
+
+			return ( isset( $settings['woocommerce_orders_integration_enabled'] ) ) && '1' === $settings['woocommerce_orders_integration_enabled'];
+		}
+
+		/**
+		 * Process the step.
+		 *
+		 * @since 1.0.0-dev
+		 *
+		 * @return bool
+		 */
+		public function process() {
+			/**
+			 * Fires when the workflow is first assigned to the billing email.
+			 *
+			 * @since 1.0.0
+			 *
+			 * @param array $entry The current entry.
+			 * @param array $form The current form.
+			 * @param array $step The current step.
+			 */
+			do_action( 'gravityflowwoocommerce_void_payment_step_started', $this->get_entry(), $this->get_form(), $this );
+
+			$order_id = gform_get_meta( $this->get_entry_id(), 'workflow_woocommerce_order_id' );
+			$order    = wc_get_order( $order_id );
+			if ( 'on-hold' === $order->get_status() ) {
+				$note = $this->get_name() . ': ' . esc_html__( 'Processed.', 'gravityflowwoocommerce' );
+				$this->add_note( $note );
+
+				// Cancel the order, so no charge will be made.
+				$note   = $this->get_name() . ': ' . esc_html__( 'Cancelled the order.', 'gravityflowwoocommerce' );
+				$result = $order->update_status( 'cancelled', $note );
+				if ( ! $result ) {
+					$note = $this->get_name() . ': ' . esc_html__( 'Failed to cancel the order. Step completed without voiding payment.', 'gravityflowwoocommerce' );
+				}
+				$this->add_note( $note );
+			} else {
+				$note = $this->get_name() . ': ' . esc_html__( 'Payment is not on hold. Step completed without voiding payment.', 'gravityflowwoocommerce' );
+				$this->add_note( $note );
+			}
+
+			return true;
+		}
+	}
+}
